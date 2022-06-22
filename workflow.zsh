d42573049cd108b138d0833886d9018010b16c1e
Nicolo Maioli <nicolomaioli@gmail.com>
2022-06-22 10:53:07 +0100

    Add kill-server alias

--- workflow.zsh
+++ workflow.zsh
@@ -2,6 +2,7 @@ alias ti='tmux new-session -d'
 alias ta='tmux a -t'
 alias tk='tmux kill-session -t'
 alias tl='tmux list-sessions'
+alias tpk='tmux kill-server'
 
 function tn {
 	FOLDER=${PWD##*/}
