50938aab56cca8451a3c9eae83d2c90dbe14dea9
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-07 15:01:22 +0100

    Make sessions awesome!

--- workflow.zsh
+++ workflow.zsh
@@ -4,18 +4,41 @@ alias tk='tmux kill-session -t'
 alias tl='tmux list-sessions'
 alias tpk='tmux kill-server'
 
-function tn {
-	FOLDER=${PWD##*/}
-	SESSION=$(print -r -- ${FOLDER:gs/\./\_})
-	SESSION_EXISTS=$(tmux list-sessions 2>/dev/null | grep $SESSION)
+function sn {
+	TARGET=$(find ~/code ~/.config -mindepth 1 -maxdepth 1 -type d | fzf)
 
-	if [ -z "$SESSION_EXISTS" ]
+	if [[ -z $TARGET ]]
 	then
-		tmux new-session -d -s $SESSION
-		tmux rename-window -t 0 'EDITOR'
-		tmux send-keys -t 'EDITOR' $EDITOR C-m
-		tmux new-window -t $SESSION:1 -n 'TERM'
+		exit 0
 	fi
 
-	tmux attach-session -t $SESSION:0
+	tn $TARGET
+}
+
+function tn {
+	if [[ $# -eq 1 ]]
+	then
+		TARGET=$1
+	else
+		TARGET=$PWD
+	fi
+
+	BASEPATH=${TARGET##*/}
+	SESSION=$(print -r -- ${BASEPATH:gs/\./\_})
+	SESSION_EXISTS=$(tmux list-sessions 2>/dev/null | grep $SESSION)
+
+	if [[ -z $SESSION_EXISTS ]]
+	then
+		tmux new-session -d -s $SESSION -c $TARGET
+		tmux rename-window -t $SESSION:0 'EDITOR'
+		tmux send-keys -t $SESSION:'EDITOR' $EDITOR C-m
+		tmux new-window -t $SESSION:1 -n 'TERM' -c $TARGET
+	fi
+
+	if [[ -z $TMUX ]]
+	then
+		tmux attach-session -t $SESSION:0
+	else
+		tmux switch -t $SESSION:0
+	fi
 }
